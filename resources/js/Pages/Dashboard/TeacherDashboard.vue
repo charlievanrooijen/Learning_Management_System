010956dac24e97bc97ca7f4cade25639f2270b00
charlievanrooijen <charlie.vanrooijen@synetic.nl>
2024-12-15 18:20:49 +0100

    Link added for teacher to create quizzes

--- resources/js/Pages/Dashboard/TeacherDashboard.vue
+++ resources/js/Pages/Dashboard/TeacherDashboard.vue
@@ -5,6 +5,10 @@ console.log('Teacher-specific dashboard');
 <template>
     <div>
         <h2 class="text-xl font-semibold">Teacher Dashboard</h2>
-        <p>Here you can create courses, quizzes, and view student progress.</p>
+        <p class="pb-3">Here you can create courses, quizzes, and view student progress.</p>
+        <a href="{{ route('quizzes.create') }}"
+            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
+            Create Quiz
+        </a>
     </div>
 </template>
